96f0db3d54581f610304c46df7997d03be574376
KHS <ewoking777@gmail.com>
2023-08-08 17:02:42 +0900

    4. 검증1 : 오류 코드와 메시지 처리5

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -10,6 +10,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -163,7 +164,8 @@ public class ValidationItemControllerV2 {
         log.info("target={}", bindingResult.getTarget());
 
         if (!StringUtils.hasText(item.getItemName())) {
-            bindingResult.rejectValue("itemName", "required");
+//            bindingResult.rejectValue("itemName", "required");
+            ValidationUtils.rejectIfEmptyOrWhitespace(bindingResult, "itemName", "required");
         }
 
         if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
