e10a79a0d3ecf6112ae60f1d71407b1058d798f6
KHS <ewoking777@gmail.com>
2023-08-09 11:34:02 +0900

    4. 검증2 : Bean Validation - 오브젝트 오류

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -5,8 +5,10 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
 @Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000")  /*jdk14 이후 버전에서는 지원X https://www.inflearn.com/questions/794097/scriptassert-%EC%97%90%EB%9F%AC-%EB%AC%B8%EC%A0%9C */
 public class Item {
 
     private Long id;
